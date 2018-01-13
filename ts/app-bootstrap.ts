03a9067993b706f5cac4d758d84f034920e56290
Saurabh Pati <saurabhpati.pati@gmail.com>
2018-01-13 20:41:25 +0530

    Fix constructor error.

--- ts/app-bootstrap.ts
+++ ts/app-bootstrap.ts
@@ -1,4 +1,6 @@
 /**
  * This is the entry point of the application and configures pieces for the app to be bootstrapped.
  */
+/// <reference path="vending-machine.ts" />
+
 let machine = new VendingMachine();
