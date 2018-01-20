47e871eb98b0186f9deca2e6c0cbd8fc1fe1f3d0
Saurabh Pati <saurabhpati.pati@gmail.com>
2018-01-20 20:15:28 +0530

    Adding payment for product

--- ts/vending-machine.ts
+++ ts/vending-machine.ts
@@ -6,9 +6,10 @@
 class VendingMachine {
     private paid: KnockoutObservable<number> = ko.observable(0);
     private acceptedMoney: Fifty[] = [new Fifty()];
-    public cells: KnockoutObservableArray<Cell> = ko.observableArray([]);
     private selectedCell: KnockoutObservable<Cell> = ko.observable(new Cell(new CocaCola()));
-
+    private canPay: KnockoutComputed<Boolean> = ko.pureComputed(() => this.paid() - this.selectedCell().product.price >= 0);
+    public cells: KnockoutObservableArray<Cell> = ko.observableArray([]);
+    
     public set Size(givenSize: VendingMachineSize) {
         this.cells([]);
 
@@ -26,6 +27,19 @@ class VendingMachine {
     public select = (cell: Cell): void => {
         cell.sold(false);
         this.selectedCell(cell);
+    }
+
+    public pay = (): void => {
+        let selectedCell: Cell = this.selectedCell();
+        
+        if (!selectedCell.stock()) {
+            alert(`Darn It! We are out of ${selectedCell.product.name}s`);
+            return;
+        }
+
+        this.paid(this.paid() - selectedCell.product.price);
+        selectedCell.stock(selectedCell.stock() - 1);
+        selectedCell.sold(true);
     }
 }
 
